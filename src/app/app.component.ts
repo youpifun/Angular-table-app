7cc1b535610d523e193beb90dca01e140ff978ca
Efim Konnov <youpifun@mail.ru>
2021-05-30 22:33:21 +0300

    Row data moved in table parent component. Added event emitter in table component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
 import { Component } from "@angular/core";
- 
+import { DataService, UsersData } from "./data.service";
+import { sortColumn, reverseColumn } from "./utils";
+
 @Component({
     selector: "table-app",
-    template: `<info-table></info-table>`
+    template: `<info-table [infoList]="infoList" (headerClicked)="sortColumnHandler($event)"></info-table>`,
+    providers: [DataService]
 })
 export class AppComponent { 
+    infoList: UsersData = [];
 
+    constructor(private dataService : DataService){}
+
+    ngOnInit() {
+        (async () => {
+            let res = await this.dataService.getUsersData();
+            Promise.all(res).then(res=>{
+                this.infoList = res;
+            });
+        })();
+    }
+    
+    sortColumnHandler($event: MouseEvent) {
+        let target = $event.target as Element;
+        if (target.classList.contains("sorted")) {
+            this.infoList = reverseColumn(this.infoList);
+            return;
+        }
+        Array.from(document.getElementsByTagName("th")).forEach(element => {
+            if (element.classList.contains("sorted")) element.classList.remove("sorted");
+        });
+        this.infoList = sortColumn(this.infoList, target.id);
+        target.classList.add("sorted");
+    }
 }
--- src/app/table/table.ts
+++ src/app/table/table.ts
@@ -1,39 +1,17 @@
-import { Component } from "@angular/core";
-import { DataService, UsersData } from "../data.service";
-import { sortColumn, reverseColumn } from "../utils";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { UsersData } from "../data.service";
+
 
 @Component({
     selector: "info-table",
     templateUrl: "./table.html",
-    styleUrls: ["./table.scss"],
-    providers: [DataService]
+    styleUrls: ["./table.scss"]
 })
 
 export class Table {
-    infoList: UsersData = [];
-
-    constructor(private dataService : DataService){}
-
-    ngOnInit() {
-        (async () => {
-           let res = await this.dataService.getUsersData();
-           Promise.all(res).then(res=>{
-            this.infoList = res;
-           });
-        })();
-    }
-    
-    sortColumnHandler($event: MouseEvent) {
-        let target = $event.target as Element;
-        if (target.classList.contains("sorted")) {
-            this.infoList = reverseColumn(this.infoList);
-            return;
-        }
-        Array.from(document.getElementsByTagName("th")).forEach(element => {
-            if (element.classList.contains("sorted")) element.classList.remove("sorted");
-        });
-        console.log($event);
-        this.infoList = sortColumn(this.infoList, target.id);
-        target.classList.add("sorted");
+    @Input() infoList: UsersData;
+    @Output() headerClicked = new EventEmitter<MouseEvent>();
+    handleHeaderClick($event: MouseEvent) {
+        this.headerClicked.emit($event);
     }
 } 
